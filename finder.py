532d102b87d75c25743896df73064dd4d34b700a
Rob Weber <robweberjr@gmail.com>
2023-02-11 16:10:28 -0600

    simplify output to a yes/no value

--- finder.py
+++ finder.py
@@ -21,13 +21,17 @@ def is_image(aFile):
     return ext.lower() in [".jpg", ".jpeg"] and not aFile.startswith('.')
 
 def compare_image(image_file, known_face_encodings):
-    print(f"Comparing against '{image_file}'")
+    result = False
     test_image = face_recognition.load_image_file(image_file)
     test_encoding = face_recognition.face_encodings(test_image)
 
     for e in test_encoding:
         results = face_recognition.compare_faces(known_face_encodings, e)
-        print(results)
+        if(results[0] == True):
+            result = True
+            break
+
+    return result
 
 # parse the arguments
 parser = argparse.ArgumentParser(description='Face Finder')
@@ -49,9 +53,13 @@ print(f"Found {len(known_face_encodings)} faces in known images")
 
 # test against unknown
 if(os.path.isfile(args.input)):
-    compare_image(args.input, known_face_encodings)
+    if(compare_image(args.input, known_face_encodings)):
+        print(f"Known face found in '{os.path.join(root, f)}'")
+    else:
+        print("No known faces found")
 else:
     for root, dirs, files in os.walk(args.input):
         for f in files:
             if(is_image(f)):
-                compare_image(os.path.join(root, f), known_face_encodings)
+                if(compare_image(os.path.join(root, f), known_face_encodings)):
+                    print(f"Known face found in '{os.path.join(root, f)}'")
