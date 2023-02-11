e7197e5ccb814b7fb9c9a5a420ee1c93107d9c4a
Rob Weber <robweberjr@gmail.com>
2023-02-11 16:04:35 -0600

    pass in image file or directory to compare

--- finder.py
+++ finder.py
@@ -10,24 +10,48 @@ def check_dir(aDir):
     else:
         raise argparse.ArgumentTypeError(f"Directory '{aDir}' does not exist")
 
+def check_exists(aFile):
+    if(os.path.exists(aFile)):
+        return aFile
+    else:
+        raise argparse.ArgumentTypeError(f"File path '{aFile}' does not exist")
+
+def is_image(aFile):
+    _, ext = os.path.splitext(aFile)
+    return ext.lower() in [".jpg", ".jpeg"] and not aFile.startswith('.')
+
+def compare_image(image_file, known_face_encodings):
+    print(f"Comparing against '{image_file}'")
+    test_image = face_recognition.load_image_file(image_file)
+    test_encoding = face_recognition.face_encodings(test_image)
+
+    for e in test_encoding:
+        results = face_recognition.compare_faces(known_face_encodings, e)
+        print(results)
+
 # parse the arguments
 parser = argparse.ArgumentParser(description='Face Finder')
 parser.add_argument('-k', '--known', default='known/', type=check_dir,
                     help='Directory of known faces to compare against')
-
+parser.add_argument("-i", "--input", type=check_exists, required=True,
+                        help="A single image, or a directory of images, to compare against")
 args = parser.parse_args()
 
+# load the known face images
 print(f"Loading known faces from '{args.known}'")
 
-known_image_encodings = []
+known_face_encodings = []
 for file in os.listdir(args.known):
     k_image = face_recognition.load_image_file(os.path.join(args.known, file))
-    known_image_encodings.append(face_recognition.face_encodings(k_image)[0])
-print(f"Found {len(known_image_encodings)} faces in known images")
+    known_face_encodings.append(face_recognition.face_encodings(k_image)[0])
 
-test_image = face_recognition.load_image_file("pics/training/2022-11-21 - Magic Kingdom Park - Main Street USA_7.jpeg")
-test_encoding = face_recognition.face_encodings(test_image)
+print(f"Found {len(known_face_encodings)} faces in known images")
 
-for e in test_encoding:
-    results = face_recognition.compare_faces(known_image_encodings, e)
-    print(results)
+# test against unknown
+if(os.path.isfile(args.input)):
+    compare_image(args.input, known_face_encodings)
+else:
+    for root, dirs, files in os.walk(args.input):
+        for f in files:
+            if(is_image(f)):
+                compare_image(os.path.join(root, f), known_face_encodings)
