2c0000dc1748ce36f969cbe211b289b3bf47d48f
Rob Weber <rweber@ecec.com>
2023-02-24 15:20:29 -0600

    replace print statements with logging

--- finder.py
+++ finder.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import os.path
 import sys
@@ -22,18 +23,17 @@ def is_image(aFile):
 
 def compare_image(image_file, known_faces):
     result = False
-    #print(image_file)
+
     if(os.path.exists(image_file)):
         test_encoding = DeepFace.find(img_path = image_file, db_path = known_faces, model_name="Facenet512", detector_backend="mtcnn", silent=True, enforce_detection=False)
 
         for df in test_encoding:
             distance = df.loc[:,"Facenet512_cosine"].min()
-            #print(distance)
             if(distance < .20):
                 result = True
                 break
     else:
-        print(f"skipping {image_file}")
+        logging.debug(f"skipping {image_file}")
 
     return result
 
@@ -45,19 +45,25 @@ parser.add_argument("-i", "--input", type=check_exists, required=True,
                     help="A single image, or a directory of images, to compare against")
 parser.add_argument("-n", "--name", type=str, default="Known face",
                     help="The name of who you're looking for, for output")
+parser.add_argument('-D', '--debug', action='store_true',
+                    help='If the program should run in debug mode')
 args = parser.parse_args()
 
-# load the known face images
-print(f"Loading faces from '{args.known}'")
+logLevel = 'INFO' if not args.debug else 'DEBUG'
+logging.basicConfig(datefmt='%m/%d %H:%M',
+                    format="%(levelname)s: %(message)s",
+                    level=getattr(logging, logLevel))
+logging.debug('Debug Mode On')
 
-#print(f"Found {len(known_face_encodings)} faces in known images")
+# load the known face images
+logging.info(f"Loading faces from '{args.known}'")
 
 # test against unknown
 if(os.path.isfile(args.input)):
     if(compare_image(args.input, args.known)):
-        print(f"{args.name} found in '{os.path.join(args.input)}'")
+        logging.info(f"{args.name} found in '{os.path.join(args.input)}'")
     else:
-        print("No known faces found")
+        logging.info("No known faces found")
 else:
     found_images = []
     for root, dirs, files in os.walk(args.input):
@@ -66,6 +72,6 @@ else:
                 if(compare_image(os.path.join(root, f), args.known)):
                     found_images.append(f"{args.name} found in '{os.path.join(root, f)}'")
 
-    print(f"Found {len(found_images)} images")
+    logging.info(f"Found {len(found_images)} images")
     for i in found_images:
-        print(i)
+        logging.info(i)
